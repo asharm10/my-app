68c1516083d9ce3246b2cdba2d81c5a854902553
Ayush Sharma <ayushsharma@Ayushs-MacBook-Air-2.local>
2024-03-26 22:42:09 -0500

    App.js changed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Cloud Computing Class!
+          Hello Cloud Computing Class!!
         </p>
       </header>
     </div>
