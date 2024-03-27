dbe493b3a1724a823caad6ed8e5ecaf9e2e4359c
Ayush Sharma <ayushsharma@Ayushs-MacBook-Air-2.local>
2024-03-26 22:05:52 -0500

    Assignment 5- React App

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './photo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello Cloud Computing Class!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
